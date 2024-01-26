b89dfdcfcd1966638d5d8eec698bf0b0a9cfc31a
강수정 <kngsujng@naver.com>
2024-01-26 12:47:52 +0900

    [Next.js 14]Next-auth로 소셜 로그인 구현 (네이버, 카카오 로그인)

--- social-login/src/app/api/auth/[...nextauth]/route.ts
+++ social-login/src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,21 @@
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
+import KakaoProvider from 'next-auth/providers/kakao';
+import NaverProvider from 'next-auth/providers/naver';
 
 export const authOptions = {
 	providers: [
 		GithubProvider({
 			clientId: process.env.GITHUB_ID || '',
 			clientSecret: process.env.GITHUB_SECRET || '',
+		}),
+		KakaoProvider({
+			clientId: process.env.KAKAO_ID || '',
+			clientSecret: process.env.KAKAO_SECRET || '',
+		}),
+		NaverProvider({
+			clientId: process.env.NAVER_ID || '',
+			clientSecret: process.env.NAVER_SECRET || '',
 		}),
 	],
 };
